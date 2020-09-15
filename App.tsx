750d82c92198770cd37576a270de7bf5cb5b83e6
Pedro Lucas <pedro_leoti@hotmail.com>
2020-09-15 18:31:22 -0300

    Add - Adição do verificador de notificações

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "react-native";
+import { StatusBar, Platform } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Linking from "expo-linking";
 
 import Routes from "./src/routes/resolver";
 import { AuthProvider } from "./src/contexts/auth";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 const App: React.FC = () => {
+  useEffect(() => {
+    Notifications.addNotificationReceivedListener(_handleNotification);
+    Notifications.addNotificationResponseReceivedListener(_handleNotificationResponse);
+  }, []);
+
+  function _handleNotification(notification) {
+    // console.log(notification.request.content.data.body);
+  }
+
+  function _handleNotificationResponse(response) {
+    const x = response.notification.request.content.data.body.x;
+    const y = response.notification.request.content.data.body.y;
+    console.log(x, y);
+    if (Platform.OS === "android") {
+      Linking.openURL(`http://maps.google.com/maps?daddr=${x},${y}`).catch((err) =>
+        console.error("An error occurred", err)
+      );
+    } else {
+      Linking.openURL(`maps://maps.apple.com/?q=${x},${y}`);
+    }
+  }
+
   return (
     <NavigationContainer>
       <AuthProvider>
